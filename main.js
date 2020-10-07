6c132a40313f6a17395b604750c91d56e1a3453d
Alvar <alvarhonsi@gmail.com>
2020-10-07 18:46:00 +0200

    commands setup

--- main.js
+++ main.js
@@ -1,10 +1,34 @@
 const Discord = require("discord.js");
+const fs = require("fs");
 
 const client = new Discord.Client();
 const { token, prefix } = require("./config.json");
 
+client.commands = new Discord.Collection();
+const commandFiles = fs
+    .readdirSync("./commands")
+    .filter((file) => file.endsWith(".js"));
+
+for (const file of commandFiles) {
+    const command = require(`./commands/${file}`);
+
+    client.commands.set(command.name, command);
+}
+
 client.once("ready", () => {
     console.log("Vaffelbot is online!");
+});
+
+client.on("message", (message) => {
+    if (!message.content.startsWith(prefix) || message.author.bot) return;
+
+    const args = message.content.slice(prefix.length).split(/ +/);
+    const command = args.shift().toLowerCase();
+
+    switch (command) {
+        case "ping":
+            client.commands.get("ping").execute(message, args);
+    }
 });
 
 client.login(token);
