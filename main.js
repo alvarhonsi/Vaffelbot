6d5c93cc90ed7148f232afb0469316ebb5c502bc
Alvar <alvarhonsi@gmail.com>
2020-10-07 17:08:07 +0200

    bot initialize

---
+++ main.js
@@ -0,0 +1,9 @@
+const Discord = require("discord.js");
+
+const client = new Discord.Client();
+
+client.once("ready", () => {
+    console.log("Vaffelbot is online!");
+});
+
+client.login("NzYzNDEzMjQ4MjM3MjQwMzQx.X33V_Q.auQ4I6laEQYfGlfkxKEj11XwbWg");
